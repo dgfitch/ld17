d3e8fbdc5102049377415077b7746ddb96a62f0e
Dan Fitch <dgfitch@gmail.com>
2010-04-24 16:40:52 -0500

    Light fx

--- README.markdown
+++ README.markdown
@@ -7,6 +7,10 @@
 
 20100424 10:36:22 - Sick. Slept a bunch, terrifying poops. Not going to worry TOO much about finishing this thing, but going to try to at least go for my tree-killing virus-spreading two-sided story concept.
 
+20100424 16:25:53 - No more time really invested so far today. Went to lunch with a friend, working out the sickness and playing some games. Getting out of the house now some, going to coffee shop it up and switch tracks to a different idea before getting too far at all on the first one, which was a bit too complicated for a 48 hour attempt where I've already spent half a day on other things.
+
+20100424 16:39:19 - Wow, the compositing and blending stuff in Pulpcore is pretty awesome. 10 minutes and the first "flashlight" or "helicopter spotlight" fx are working.
+
 
 ## Vim notes
 
--- src/LD17.java
+++ src/LD17.java
@@ -1,5 +1,6 @@
 import pulpcore.CoreSystem;
 import pulpcore.image.CoreFont;
+import pulpcore.image.BlendMode;
 import pulpcore.Input;
 import pulpcore.scene.Scene2D;
 import pulpcore.sound.Sound;
@@ -7,28 +8,51 @@ import pulpcore.sprite.ImageSprite;
 import pulpcore.sprite.Label;
 import pulpcore.sprite.Sprite;
 import pulpcore.sprite.FilledSprite;
+import pulpcore.sprite.Group;
+import static pulpcore.image.Colors.*;
 
 public class LD17 extends Scene2D {
     
     Label theme;
     ImageSprite tam;
+    ImageSprite cursor;
+    Group maskLayer;
+    Group itemLayer;
+    Group backLayer;
     
     @Override
     public void load() {
-        add(new ImageSprite("background.png", 0, 0));
+        setCursor(Input.CURSOR_OFF);
+        cursor = new ImageSprite("light_glow_1.png", 0, 0);
+        cursor.setAnchor(0.5, 0.5);
+        cursor.visible.set(false);
 
-        add(new ImageSprite("tile_grass_1.png", 0, 0));
-        add(new ImageSprite("tile_grass_1.png", 64, 0));
-        add(new ImageSprite("tile_grass_1.png", 0, 64));
-        add(new ImageSprite("tile_grass_1.png", 64, 64));
+        maskLayer = new Group();
+        maskLayer.add(new FilledSprite(gray(18)));
+        maskLayer.add(cursor);
+        maskLayer.setBlendMode(BlendMode.Multiply());
+        maskLayer.createBackBuffer();
+
+        backLayer = new Group();
+        backLayer.add(new ImageSprite("background.png", 0, 0));
+
+        itemLayer = new Group();
+        itemLayer.add(new ImageSprite("tile_grass_1.png", 0, 0));
+        itemLayer.add(new ImageSprite("tile_grass_1.png", 64, 0));
+        itemLayer.add(new ImageSprite("tile_grass_1.png", 0, 64));
+        itemLayer.add(new ImageSprite("tile_grass_1.png", 64, 64));
 
         tam = new ImageSprite("tam.png", 0, 0);
-        add(tam);
+        itemLayer.add(tam);
         
         CoreFont font = CoreFont.load("hello.font.png");
         theme = new Label(font, "HOORAY!!", 320, 50);
         theme.setAnchor(0.5, 0.5);
-        add(theme);
+        itemLayer.add(theme);
+
+        addLayer(backLayer);
+        addLayer(itemLayer);
+        addLayer(maskLayer);
         
         //// TODO: Not working
         //Sound sound = Sound.load("sound.wav");
@@ -37,6 +61,9 @@ public class LD17 extends Scene2D {
     
     @Override
     public void update(int elapsedTime) {
+        cursor.setLocation(Input.getMouseX(), Input.getMouseY());
+        cursor.visible.set(Input.isMouseInside());
+
         theme.y.animateTo(640 - Input.getMouseY(), 1000);
 
         tam.x.animateTo(Input.getMouseX() - 320, 300);
